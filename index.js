399e3987411afdbd7bd1496bbbf4a3fbe86892c9
JJS4M <sam_jj72@yahoo.com>
2022-11-08 06:54:02 +0800

    updated index.js + added ATTLH

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ async function index_onload() {
 	var response = await fetch('pages.json');
 	var pages = await response.json();
 	var container = document.getElementById('container');
-	
+	container.appendChild(document.createElement('br'));
+	container.appendChild(document.createElement('br'));
 	for (const page of pages) {
 		const hyperlink = document.createElement('a');
 		hyperlink.href = page.location;
 		hyperlink.textContent = page.name;
 		container.appendChild(hyperlink);
+		container.appendChild(document.createElement('br'));
+		container.appendChild(document.createElement('br'));
 	}
 }
