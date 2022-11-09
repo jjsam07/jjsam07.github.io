c6e3d41cdf4d829750d68223e115457cf7e08d78
JJS4M <sam_jj72@yahoo.com>
2022-11-09 23:46:37 +0800

    added loading screen

--- attlh/script.js
+++ attlh/script.js
@@ -4,8 +4,8 @@ function delay(ms) {
 
 async function decompress_blob(compressed_blob) {
   const ds = new DecompressionStream("gzip");
-  const decompressedStream = compressed_blob.stream().pipeThrough(ds);
-  return await new Response(decompressedStream).blob();
+  const decompressed_stream = compressed_blob.stream().pipeThrough(ds);
+  return await new Response(decompressed_stream).blob();
 }
 
 
@@ -109,12 +109,6 @@ async function fetch_frame_bundles(frame_bundle) {
 	}
 }
 
-async function frame_unbundle(frame_bundle, frame_array) {
-	for (let i = 1; i < 11; i++) {
-		stream_decode(await frame_bundle[i], frame_array);
-	}
-}
-
 async function decode_frames(decoded_frame, frame_array) {
 	frame_buffer = frame_array[0];
 	decoded_frame.push(framedata_to_image(frame_buffer));
@@ -122,6 +116,54 @@ async function decode_frames(decoded_frame, frame_array) {
 		frame_update(frame_buffer, frame_array[i]);
 		decoded_frame.push(framedata_to_image(frame_buffer));
 		await delay(1);
+	}
+}
+
+async function show_loading_screen(output_element, is_loading) {
+	var loading_str = 'Loading';
+	var loading_char_code = [0x4c, 0x6f, 0x61, 0x64, 0x69, 0x6e, 0x67];
+	var Lloading = ['   ', '   ', '   ', '.  ', '.. ', '...', ' ..', '  .'];
+	var Rloading = ['.  ', '.. ', '...', ' ..', '  .', '   ', '   ', '   '];
+	var fade = []
+	var temp_str = ''
+	var char_code;
+	var counter = 0;
+	for (let i = 0; i < 7; i++) {
+		await delay(50);
+		char_code = (Math.floor(Math.random() * 100) % 0x5F) + 0x20;
+		fade.push(char_code);
+		temp_str += String.fromCharCode(char_code);
+		output_element.textContent = temp_str;
+	}
+	while (true) {
+		for (let j = 0; j < 7; j++) {
+			if (fade[j] > loading_char_code[j]) {
+				fade[j] -= 1;
+				counter += 1;
+			} else if (fade[j] < loading_char_code[j]) {
+				fade[j] += 1;
+				counter += 1;
+			}
+		}
+		temp_str = '';
+		for (const c of fade) {
+			temp_str += String.fromCharCode(c);
+		}
+		output_element.textContent = temp_str;
+		if (counter === 0) {
+			break;
+		} else {
+			counter = 0;
+		}
+		await delay(10);
+	}
+	while (is_loading[0]) {
+		await delay(1000);
+		for (let k = 0; k < 8; k++) {
+			output_element.textContent = Lloading[k] + loading_str + Rloading[k];
+			await delay(250);
+		}
+		output_element.textContent = '   ' + loading_str + '   ';
 	}
 }
 
@@ -134,28 +176,36 @@ async function attlh() {
 	var frame_array = [];
 	var frame_buffer;
 	var decoded_frame = [];
+	var loading = [true];
 	var start;
 	var children;
 	var j = 0;
 	var p;
 	var str;
+	// Show loading screen
+	p = document.createElement('p');
+	p.setAttribute('style', 'font-size: 30px;');
+	p.appendChild(document.createTextNode(''));
+	display.appendChild(p);
+	show_loading_screen(p, loading)
+	// Fetch frame bundles
+	fetch_frame_bundles(frame_bundle);
+	// Unbundle to individual frames
+	for (let j = 0; j < 11; j++) {
+		stream_decode(await frame_bundle[j], frame_array);
+	}
+	// Convert frames to displayable image (or in this case, text)
+	decode_frames(decoded_frame, frame_array);
+	loading[0] = false;
+	while (display.hasChildNodes()) {
+		display.removeChild(display.firstChild)
+	}
 	// Create display-output elements
 	for (let i = 0; i < height; i++) {
 		p = document.createElement('p');
 		p.appendChild(document.createTextNode(''));
 		display.appendChild(p);
 	}
-	// Fetch frame bundles
-	fetch_frame_bundles(frame_bundle);
-	await delay(1000);
-	stream_decode(await frame_bundle[0], frame_array);
-	await delay(1500);
-	// Unbundle to individual frames
-	frame_unbundle(frame_bundle, frame_array)
-	
-	// Convert frames to displayable image (or in this case, text)
-	decode_frames(decoded_frame, frame_array);
-	await delay(2500);
 	children = display.children;
 	audio.play();
 	start = Date.now();
