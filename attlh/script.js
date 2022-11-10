b22ed45937a045d198916ce8df9017110c58d4ce
JJS4M <sam_jj72@yahoo.com>
2022-11-10 20:00:03 +0800

    Update script.js

--- attlh/script.js
+++ attlh/script.js
@@ -206,6 +206,7 @@ async function attlh() {
 	}
 	// Fetch audio
 	audio.oncanplaythrough = () => { loading_end(); }
+	audio.addEventListener('oncanplaythrough', () => { loading_end(); });
 	audio.src = 'audio.m4a';
 	while (loading[0]) { await delay(1); } // wait for audio to be fully loaded
 	await delay(2000);
